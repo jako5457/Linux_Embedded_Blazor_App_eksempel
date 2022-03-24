a0044af58cba4618e25521f0d663fb6b97beddee
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-03-24 09:39:19 +0100

    Add project files.

---
+++ ServiceLayer/Dto/HumidityDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Dto
+{
+    public class HumidityDto
+    {
+
+        public DateTime Time { get; set; }
+
+        public int Humidity { get; set; }
+
+        public string Host { get; set; } = string.Empty;
+
+    }
+}
---
+++ ServiceLayer/Dto/TempratureDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Dto
+{
+    public class TempratureDto
+    {
+        public DateTime Time { get; set; }
+
+        public double TempratureC { get; set; }
+
+        public string Host { get; set; } = string.Empty;
+    }
+}
