ff2ffde10d4fb3a11bfb6353374ddeb143649d9f
jakob. spiel .køhler <jakobspielkohler@live.dk>
2022-03-24 12:38:48 +0100

    Oprettet HumidityService

--- ServiceLayer/Dto/HumidityDto.cs
+++ ServiceLayer/Dto/HumidityDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InfluxDB.Client.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,13 @@ namespace ServiceLayer.Dto
 {
     public class HumidityDto
     {
-
+        [Column("time")]
         public DateTime Time { get; set; }
 
+        [Column("value")]
         public int Humidity { get; set; }
 
-        public string Host { get; set; } = string.Empty;
-
+        [Column("tag")]
+        public string tag { get; set; } = string.Empty;
     }
 }
---
+++ ServiceLayer/MeasumentTimeSpan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public enum MeasumentTimeSpan
+    {
+        seconds = 0,
+        minutes = 1,
+        hours = 2,
+        days = 3,
+        months = 4,
+        years = 5,
+    }
+}
